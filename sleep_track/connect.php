d2bd15c905cccc2c55a7e8530096b6ba0e73df48
Nithi67160346 <67160346@go.buu.ac.th>
2025-10-31 19:51:40 +0700

    Update connect.php

--- sleep_track/connect.php
+++ sleep_track/connect.php
@@ -1,9 +1,9 @@
 <?php
 // connect.php
 $DB_HOST = "localhost";
-$DB_NAME = "s67160346";          // <<< เปลี่ยนเป็นฐานของคุณ
-$DB_USER = "s67160346";          // <<< ผู้ใช้ MySQL
-$DB_PASS = "ccKWeE31";   // <<< รหัสผ่าน
+$DB_NAME = "";          // <<< เปลี่ยนเป็นฐานของคุณ
+$DB_USER = "";          // <<< ผู้ใช้ MySQL
+$DB_PASS = "";   // <<< รหัสผ่าน
 
 $conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 if (!$conn) {
@@ -12,3 +12,4 @@ if (!$conn) {
   exit("DB connect error: " . mysqli_connect_error());
 }
 mysqli_set_charset($conn, "utf8mb4");
+
